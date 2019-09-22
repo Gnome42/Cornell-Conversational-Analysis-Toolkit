fe7cd43ad5bc20a0428e40b966e4d74fcc4b0a23
June Cho <sc782@cornell.edu>
2019-09-22 17:40:10 -0400

    Added vocab overlap and sentiment transformers

--- convokit/__init__.py
+++ convokit/__init__.py
@@ -7,4 +7,6 @@ from .transformer import *
 from .parser import *
 from .hyperconvo import *
 from .conversationBalance import *
+from .vocabOverlap import *
+from .firstImpression import *
 #__path__ = __import__('pkgutil').extend_path(__path__, __name__)
