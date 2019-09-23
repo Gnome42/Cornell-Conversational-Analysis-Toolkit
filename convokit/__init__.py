458c60dd373eda5251ea6ef9943891d3e1f88512
Noam Eshed <noam_eshed@yahoo.com>
2019-09-23 16:15:18 -0400

    updated __init__ for DAMSLScores and updates E2 description for statement balance

--- convokit/__init__.py
+++ convokit/__init__.py
@@ -9,4 +9,5 @@ from .hyperconvo import *
 from .conversationBalance import *
 from .vocabOverlap import *
 from .firstImpression import *
+from .damslScores import *
 #__path__ = __import__('pkgutil').extend_path(__path__, __name__)
