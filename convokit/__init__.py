bc745145c75d1134393c3db2ca174aee1d613acb
sc782 <sc782@cornell.edu>
2019-09-24 20:14:10 -0400

    renamed firstImpression transformer to vaderSentiment

--- convokit/__init__.py
+++ convokit/__init__.py
@@ -8,6 +8,6 @@ from .parser import *
 from .hyperconvo import *
 from .conversationBalance import *
 from .vocabOverlap import *
-from .firstImpression import *
+from .vaderSentiment import *
 from .damslScores import *
 #__path__ = __import__('pkgutil').extend_path(__path__, __name__)
